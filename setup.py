eefa0100d93894ff45bd166e3259082aee0aa001
John Casey <jdcasey@commonjava.org>
2019-02-11 14:22:17 -0600

    add click requirement

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
     version="0.1",
     long_description="This is a utility for promoting content in the Indy package repository manager",
     classifiers=[
-      "Development Status :: 3 - Alpha",
-      "Intended Audience :: Developers",
-      "License :: OSI Approved :: Apache Public License",
-      "Programming Language :: Python :: 3",
-      "Topic :: Software Development :: Build Tools",
-      "Topic :: Utilities",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Public License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Utilities",
     ],
     keywords='indy maven build java',
     author='John Casey',
@@ -21,14 +21,15 @@ setup(
     url='https://github.com/Commonjava/indy_build_promote',
     license='APLv2',
     install_requires=[
-      'requests',
-      'ruamel.yaml',
+        'click',
+        'requests',
+        'ruamel.yaml',
     ],
     entry_points={
-      'console_scripts': [
-        'consolidate-builds = indy_build_promote:promote',
-        'list-build-files = indy_build_promote:list_build_files'
-      ],
+        'console_scripts': [
+            'consolidate-builds = indy_build_promote:promote',
+            'list-build-files = indy_build_promote:list_build_files'
+        ],
     }
 )
 
