75d23408aa75c4332a55e14a3c714d2c47a35a5d
John Casey <jdcasey@commonjava.org>
2019-02-11 14:52:51 -0600

    simplify click annotations for now

--- indy_build_promote/commands.py
+++ indy_build_promote/commands.py
@@ -5,20 +5,20 @@ from indy_build_promote.promote import promote_builds
 from indy_build_promote.builds import list_builds
 
 @click.command()
-@click.option('-c', '--config', default=DEFAULT_CONFIG_FILE, help='Alternative configuration file', type=click.File('r'))
-@click.argument('input_file', required=True, help='File containing the list of hosted repositories whose content you wish to promote')
-@click.argument('progress_file', required=True, help='File containing the list of repos successfully promoted')
-@click.argument('fail_file', required=True, help='File containing the list of repos we could not promote')
+@click.option('-c', '--config', default=DEFAULT_CONFIG_FILE)
+@click.argument('input_file')
+@click.argument('progress_file')
+@click.argument('fail_file')
 def promote(config, input_file, progress_file, fail_file):
-	cfg = load(config_file)
+	cfg = load(config)
 	promote_builds(cfg, input_file, progress_file, fail_file)
 
 @click.command()
-@click.option('-c', '--config', default=DEFAULT_CONFIG_FILE, help='Alternative configuration file', type=click.File('r'))
-@click.argument('input_file', required=True, help='File containing the list of hosted repositories whose content you wish to list')
-@click.argument('progress_file', required=True, help='File containing the list of repos successfully listed')
-@click.argument('repo_defs', required=True, help='Directory where repository definition JSON is stored')
-@click.argument('build_listing_dir', required=True, help='Directory where listings should be written')
+@click.option('-c', '--config', default=DEFAULT_CONFIG_FILE)
+@click.argument('input_file')
+@click.argument('progress_file')
+@click.argument('repo_defs')
+@click.argument('build_listing_dir')
 def list_build_files(config, input_file, progress_file, repo_defs, build_listing_dir):
 	cfg = load(config)
 	list_builds(cfg, input_file, progress_file, repo_defs, build_listing_dir)
